cd41f860b53a27002672a1b7f66c574ddab06ae5
gystemd <gystemd@gmail.com>
2023-11-19 15:56:06 +0100

    update net to Mumbai

--- src/veramo/setup.ts
+++ src/veramo/setup.ts
@@ -6,7 +6,7 @@ import { getResolver as webDidResolver } from 'web-did-resolver'
 import { createAgent, IResolver } from '@veramo/core'
 import { CredentialIssuerEIP712, } from "@veramo/credential-eip712";
 
-const providerConfig = { name: '0x539', rpcUrl: 'http://localhost:8545', registry: '0xf48263bfc8237ae29480280083c63ba2c446eafa' }
+const providerConfig = { name: '0x13881', rpcUrl: 'https://rpc-mumbai.maticvigil.com', registry: '0xdca7ef03e98e0dc2b855be647c39abe984fcf21b' }
 
 export const agent = createAgent<IResolver>({
     plugins: [
