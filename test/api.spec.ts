d0fd1106430e48acd6493ee5a8f9f5f0a4858243
gystemd <gystemd@gmail.com>
2023-11-19 17:03:13 +0100

    add tests for VP verification

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -16,7 +16,6 @@ describe("degree verification", () => {
 		}
 		const result = await agent.verifyPresentation(args);
 		expect(result.verified).toBe(true);
-		// return request(app).post("/send").expect(200);
 	});
 });
 
@@ -28,6 +27,82 @@ describe("vaccine and degree verification", () => {
 		}
 		const result = await agent.verifyPresentation(args);
 		expect(result.verified).toBe(true);
-		// return request(app).post("/send").expect(200);
 	});
+});
+
+describe("fail verification - change credential value", () => {
+	it("should fail to verify the VP", async () => {
+		const payload = validPhdDegreePresentation;
+
+		// change the degree level to be a bachelor; This invalidates the VP
+		const invalid_vc = payload.verifiableCredential[0].replace("PhD", "Bachelor");
+		const invalid_presentation = {
+			...payload,
+			verifiableCredential: [invalid_vc]
+		}
+
+		const args: IVerifyPresentationEIP712Args = {
+			presentation: invalid_presentation
+		}
+
+		const result = await agent.verifyPresentation(args);
+		expect(result.verified).toBe(false);
+	})
+});
+
+describe("fail verification - change issuer", () => {
+	it("should to verify the VP", async () => {
+		const payload = validPhdDegreePresentation;
+		const invalid_vc = payload.verifiableCredential[0].replace("0x8aadd53dc222d0490f0804a31792a635922a43a8", "0x8aadd53dc222d0490f0804a31792a635922a43a9");
+		const invalid_presentation = {
+			...payload,
+			verifiableCredential: [invalid_vc]
+		}
+
+		const args: IVerifyPresentationEIP712Args = {
+			presentation: invalid_presentation
+		}
+
+		const result = await agent.verifyPresentation(args);
+		expect(result.verified).toBe(false);
+	})
+});
+
+describe("fail verification - change issuer", () => {
+	it("should to verify the VP", async () => {
+		const payload = validPhdDegreePresentation;
+		const invalid_vc = payload.verifiableCredential[0].replace("0x8aadd53dc222d0490f0804a31792a635922a43a8", "0x8aadd53dc222d0490f0804a31792a635922a43a9");
+		const invalid_presentation = {
+			...payload,
+			verifiableCredential: [invalid_vc]
+		}
+
+		const args: IVerifyPresentationEIP712Args = {
+			presentation: invalid_presentation
+		}
+
+		const result = await agent.verifyPresentation(args);
+		expect(result.verified).toBe(false);
+	})
+});
+
+describe("fail verification - change proofValue", () => {
+	it("should fail to verify the VP", async () => {
+		const payload = validPhdDegreePresentation;
+		const invalid_proof = {
+			...payload.proof,
+			proofValue: "0x0"
+		}
+		const invalid_presentation = {
+			...payload,
+			proof: invalid_proof
+		}
+
+		const args: IVerifyPresentationEIP712Args = {
+			presentation: invalid_presentation
+		}
+
+		const result = await agent.verifyPresentation(args);
+		expect(result.verified).toBe(false);
+	})
 });
