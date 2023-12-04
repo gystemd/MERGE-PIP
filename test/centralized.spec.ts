280bc27e6b4c44fc1a8eaa567e49461af6e0639c
gystemd <gystemd@gmail.com>
2023-12-04 10:08:10 +0100

    complete tests for centralized version

--- test/centralized.spec.ts
+++ test/centralized.spec.ts
@@ -20,7 +20,83 @@ describe("test access to research-paper-computer-science resource", () => {
         const response = await axios.post('http://localhost:8080/evaluate', request, {
             headers: { 'Content-Type': 'application/json' }
         });
+        expect(response.data).toBe(true);
+    });
+});
+
+describe("test access to the conference resource", () => {
+    it("should return 200", async () => {
+        let resource = "conference-centralized";
+        let request: string = `<Request xmlns="urn:oasis:names:tc:xacml:3.0:core:schema:wd-17" `
+            + `CombinedDecision="false" `
+            + `ReturnPolicyIdList="true">`
+            + `<Attributes Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject">`
+            + `<Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id" IncludeInResult="false">`
+            + `<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">` + "1" + `</AttributeValue>`
+            + `</Attribute>`
+            + `</Attributes>`
+            + `<Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource">`
+            + `<Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:resource:resource-id" IncludeInResult="true">`
+            + `<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">` + resource + `</AttributeValue>`
+            + `</Attribute>`
+            + `</Attributes>`;
+        request += `</Request>`;
+        const response = await axios.post('http://localhost:8080/evaluate', request, {
+            headers: { 'Content-Type': 'application/json' }
+        });
+        expect(response.data).toBe(true);
+    });
+});
+
+
+describe("test access to the conference resource", () => {
+    it("should return 200", async () => {
+        let resource = "course-material-centralized";
+        let request: string = `<Request xmlns="urn:oasis:names:tc:xacml:3.0:core:schema:wd-17" `
+            + `CombinedDecision="false" `
+            + `ReturnPolicyIdList="true">`
+            + `<Attributes Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject">`
+            + `<Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id" IncludeInResult="false">`
+            + `<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">` + "2" + `</AttributeValue>`
+            + `</Attribute>`
+            + `</Attributes>`
+            + `<Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource">`
+            + `<Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:resource:resource-id" IncludeInResult="true">`
+            + `<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">` + resource + `</AttributeValue>`
+            + `</Attribute>`
+            + `</Attributes>`;
+        request += `</Request>`;
+        const response = await axios.post('http://localhost:8080/evaluate', request, {
+            headers: { 'Content-Type': 'application/json' }
+        });
         // check if the property authorized is true
         expect(response.data).toBe(true);
     });
 });
+
+
+describe("test access to the coding course", () => {
+    it("should return 200", async () => {
+        let resource = "coding-course-centralized";
+        let request: string = `<Request xmlns="urn:oasis:names:tc:xacml:3.0:core:schema:wd-17" `
+            + `CombinedDecision="false" `
+            + `ReturnPolicyIdList="true">`
+            + `<Attributes Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject">`
+            + `<Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id" IncludeInResult="false">`
+            + `<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">` + "3" + `</AttributeValue>`
+            + `</Attribute>`
+            + `</Attributes>`
+            + `<Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource">`
+            + `<Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:resource:resource-id" IncludeInResult="true">`
+            + `<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">` + resource + `</AttributeValue>`
+            + `</Attribute>`
+            + `</Attributes>`;
+        request += `</Request>`;
+        const response = await axios.post('http://localhost:8080/evaluate', request, {
+            headers: { 'Content-Type': 'application/json' }
+        });
+        // check if the property authorized is true
+        expect(response.data).toBe(true);
+    });
+});
+
